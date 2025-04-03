770a9373624346bde18619bdefb8537716303483
Roman <didukhroma@gmail.com>
2025-04-03 18:57:03 +0200

    task_2 done

--- task_2.py
+++ task_2.py
@@ -1,3 +1,13 @@
 from collections import deque
 
 def is_palindrom(string: str) -> bool:
+    queue = deque(string.lower().replace(' ', ''))
+    while len(queue) > 1:
+        if  queue.popleft() != queue.pop():
+            return False
+    return True
+
+
+print(is_palindrom("A man, a plan, a canal: Panama"))
+print(is_palindrom("race a car"))
+print(is_palindrom("0P0"))
