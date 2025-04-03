ea7320224eeec76ce55863a3d50f86dcdf547744
Roman <didukhroma@gmail.com>
2025-04-03 16:10:21 +0200

    hw task1 done

---
+++ task_3.py
@@ -0,0 +1,23 @@
+from queue import Queue
+from datetime import datetime
+import uuid
+import random
+
+queue = Queue()
+
+def generate_request():
+    new_request = {"request_id": f"{str(uuid.uuid4())}-{datetime.timestamp(datetime.now())}"}
+    queue.put(new_request)
+    print(new_request)
+
+def process_request():
+    if not queue.empty():
+        request = queue.get()
+        print(request)
+
+for i in range(10):
+    generate_request()
+    if random.randint(0, 1) == 1:
+        print("Processing request")
+        process_request()
+
